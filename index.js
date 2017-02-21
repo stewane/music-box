ac075ea9a51377d89c76ec73cf392fcd322bb8da
stephane.b <stewane90@gmail.como>
2017-02-21 10:46:43 +0100

    ajout music box

---
+++ index.js
@@ -0,0 +1,43 @@
+var rectangle1= document.getElementById("rectangle1");
+var son1= document.getElementById("son1")
+rectangle1.onclick = function (){
+	son1.play();
+
+}
+
+
+var rectangle2= document.getElementById("rectangle2");
+var son2= document.getElementById("son2")
+rectangle2.onclick = function (){
+	son2.play();
+
+}
+
+
+var rectangle3= document.getElementById("rectangle3");
+var son3= document.getElementById("son3")
+rectangle3.onclick = function (){
+	son3.play();
+
+}
+
+
+var rectangle4= document.getElementById("rectangle4");
+var son4= document.getElementById("son4")
+rectangle4.onclick = function (){
+	son4.play();
+
+}
+
+var rectangle5= document.getElementById("rectangle5");
+var son5= document.getElementById("son5")
+rectangle5.onclick = function (){
+	son5.play();
+
+}
+
+var rectangle6= document.getElementById("rectangle6");
+var son6= document.getElementById("son6")
+rectangle6.onclick = function (){
+	son6.play();
+}
